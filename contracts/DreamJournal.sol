57ec4229a61185593e1ad7e501cf69e8cd738448
MalcolmLattimore <gpradllq560620@outlook.com>
2025-11-17 09:57:00 +0000

    feat: emit DreamContentLength event on content length query
    - Emit event when content length is accessed
    - Enable off-chain analytics and monitoring
    - Track dream content access patterns

--- contracts/DreamJournal.sol
+++ contracts/DreamJournal.sol
@@ -92,10 +92,11 @@ contract DreamJournal is SepoliaConfig {
     /// @notice Get the length of FHE encrypted content for a dream
     /// @param id The dream ID
     /// @return length Number of FHE encrypted bytes (characters)
-    function getDreamContentLength(uint256 id) external view returns (uint256 length) {
+    function getDreamContentLength(uint256 id) external returns (uint256 length) {
         Dream storage dream = _dreams[id];
         require(dream.encContent.length > 0, "Dream does not exist");
-        return dream.encContent.length;
+        length = dream.encContent.length;
+        emit DreamContentLength(id, length);
     }
 
     /// @notice Get total number of dreams stored in the contract
